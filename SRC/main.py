9fa619a92430eb35f1c3cec723e164223918c7cb
tdufty <tom.dufty@hotmail.com>
2018-08-15 11:44:35 +1000

    intial

---
+++ SRC/main.py
@@ -0,0 +1,26 @@
+import main_ui
+
+#main variables
+
+tracked_dir = []
+
+
+#open trackfile in install directory
+def read_trackfile():
+    print('reading trackfile')
+
+#open sqlite and read document register
+def read_doc_register(tracked_dir):
+    print('reading document register')
+
+
+def create_doc_register():
+    print('new document register created')
+
+# prepoluate tree
+
+
+
+
+def add_tracked_file():
+    print('new file tracked')
