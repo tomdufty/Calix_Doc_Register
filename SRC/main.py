5d59b879049ac6a934b34a6a2414b27a3851a7ef
tdufty <tom.dufty@hotmail.com>
2018-08-15 13:24:02 +1000

    High level global vairbvales added

--- SRC/main.py
+++ SRC/main.py
@@ -1,4 +1,13 @@
 import main_ui
+import sqlite3
+
+#global variables
+FEILDS_LIST = ['discipline','size','project','seqno','ver','desc','auth']
+DISCIPLINE_CODES = ['A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z']
+DISCIPLINE_LIST = ['Architechtural','Process','Civil','Engineering Administration','Electrical','Finance/Accounting',
+                   'General','Health, Safety and Environmental','Instrumentation and Control', 'Building', 'Manuals',
+                   'Mechanical','Procurement','Piping','Quality Assurance/Control','MDR Documentation','Structrual',
+                   '','','Vendor','','Client','','']
 
 #main variables
 
@@ -18,9 +27,14 @@ def create_doc_register():
     print('new document register created')
 
 # prepoluate tree
+def populate_tree():
+    print('populating filee tree')
 
+def populate_form():
+    print('popoulating form')
 
 
 
 def add_tracked_file():
     print('new file tracked')
+
